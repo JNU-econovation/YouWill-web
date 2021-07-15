5e9d490176e5717cded1cae1e949747bea9fad9c
GlassK <glassk0724@gmail.com>
2021-07-16 05:36:25 +0900

    chore: 프로젝트 디렉토리 구조 변경

--- public/dist/js/will/will.js
+++ public/dist/js/will/will.js
@@ -1,30 +1,30 @@
-
 const triggerMargin = 150;
-const potos = document.querySelectorAll('.poto');
+const potos = document.querySelectorAll('.image');
 const bottom = document.querySelector('.bottom');
 
-const setLayout = function() {
+const setLayout = function () {
   for (const element of potos) {
     if (!element.classList.contains('show')) {
-      if (window.innerHeight > element.getBoundingClientRect().top + triggerMargin) {
+      if (
+        window.innerHeight >
+        element.getBoundingClientRect().top + triggerMargin
+      ) {
         element.classList.add('show');
       }
     }
   }
-  if(!bottom.classList.contains('show')){
-        if(window.innerHeight > bottom.getBoundingClientRect().top){
-            bottom.classList.add('show');
-        }
-          
+  if (!bottom.classList.contains('show')) {
+    if (window.innerHeight > bottom.getBoundingClientRect().top) {
+      bottom.classList.add('show');
     }
-  
-}
+  }
+};
 // const setLayoutBottom= function(){
 //     if(!bottom.classList.contains('show')){
 //         if(window.innerHeight > bottom.getBoundingClientRect().top){
 //             bottom.classList.add('show');
 //         }
-      
+
 //     }
 // }
 
