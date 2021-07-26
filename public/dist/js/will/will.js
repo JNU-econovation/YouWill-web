fcbfa6abe6e68a2b9181ffa45f7b89edc896788e
unknown <hbapt1203@gmail.com>
2021-07-26 17:30:08 +0900

    fix: 유서 토스트메시지 error수정

--- public/dist/js/will/will.js
+++ public/dist/js/will/will.js
@@ -27,22 +27,4 @@ const setLayout = function () {
 window.addEventListener('load', setLayout);
 window.addEventListener('scroll', setLayout);
 
-//////////////////////////////////
-const toast=document.querySelector('.toast');
-const endBtn=document.querySelector('.yellow-button');
-const centered=document.getElementById('centered');
-const closeBtn=document.querySelector('.close');
-let clicked;
 
-endBtn.addEventListener('click',function(e){
-  toast.classList.add('show');
-  centered.style.opacity=0.5;
-  centered.style.pointerEvents='none';
-})
-closeBtn.onclick=function(){
-  toast.classList.remove('show');
-  centered.style.opacity=1;
-  centered.style.pointerEvents='auto';
-  window.location.href = 'will_detail.html';
-
-}
